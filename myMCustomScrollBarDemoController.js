e8fe4c6714ed5fedecdae69d1d74e2fb4040e721
pawanIngale <pawaningale93@gmail.com>
2017-08-05 15:18:24 +0530

    Add files via upload

---
+++ myMCustomScrollBarDemoController.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="myMCustomScrollBarDemoDirective.js" />
+var myMCustomScrollBarDemo = angular.module('myMCustomScrollBarDemo', ['ngMCustomScrollBar']);
+myMCustomScrollBarDemo.controller('myMCustomScrollBarDemoController', ['$scope', '$compile', '$http', function ($scope, $compile, $http) {
+    $scope.Items = [];
+    $scope.getScrollData = function () {
+        for (var i = 0; i <= Math.random() ; i++) {
+            $scope.Items.push(i);
+        }
+        //$http.get('http://localhost:54845/api/ScrollBarDataAPI/scrollBarData').success(function (data) {
+        $('#abc').addClass('customClass');
+        //$scope.Items = data;
+        //}).error(function (err) {
+        //    console.log(err);
+        //});
+    }
+    $scope.getScrollData();
+    $scope.configScroll = { scrollButtons: { enable: false }, theme: "dark", live: "once" };
+    $scope.configScroll1 = { scrollButtons: { enable: true }, theme: "dark", live: "once" };
+}]);
